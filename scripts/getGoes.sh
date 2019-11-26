a59614507d67a861af26e63245881e64b45580e0
Phil Rosenberg <p.d.rosenberg@gmail.com>
2019-11-26 15:53:52 +0000

    Modified the getGoes script to accept the product and destination as command line arguments

--- scripts/getGoes.sh
+++ scripts/getGoes.sh
@@ -5,14 +5,40 @@
 #and postfix
 #set start and end dates and destination then run the script
 #at an appropriate interval using cron
-export PREFIX=https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/car/GEOCOLOR/
-export POSTFIX=_GOES16-ABI-car-GEOCOLOR-4000x4000.jpg
+
+if [ $# -eq 1 ]
+then
+  if [ "$1" == "--help" ] || [ "$1" == "-h" ]
+  then
+    echo Usage: getGoes.sh product destination_directory
+    echo product may be any of GEOCOLOR, AirMass, Sandwich, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16
+    echo destination_directory is the location to save the images
+    exit 0
+  else
+    echo Incorrect number of parameters. Run getGoes.sh --help to see usage information
+    exit 1
+  fi
+fi
+
+if [ $# -ne 2 ]
+then
+  echo Incorrect number of parameters. Run getGoes.sh --help to see usage information
+  exit 1
+fi
+
+
+export PRODUCT=$1
+#export PRODUCT=GEOCOLOR
+export PREFIX=https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/car/$PRODUCT/
+export POSTFIX=_GOES16-ABI-car-$PRODUCT-4000x4000
+export EXTENSION=jpg
 
 #yyyyjjjhhmm jjj is day of year
 export START=20193261300
 export END=20210010000
 
-export DESTINATION=$HOME/Documents/Work/GOES
+export DESTINATION=$2
+#export DESTINATION=$HOME/GOES
 
 
 
@@ -36,7 +62,8 @@ do
     do
       for MINUTE in $MINUTES
       do
-        export FILE=$YEAR$DAY$HOUR$MINUTE$POSTFIX
+        export FILE=$YEAR$DAY$HOUR$MINUTE$POSTFIX.$EXTENSION
+        export TEMPFILE=$YEAR$DAY$HOUR$MINUTE$POSTFIX-temp.$EXTENSION
         #echo $FILE
         export URL=$PREFIX$FILE
         if [ $YEAR$DAY$HOUR$MINUTE -gt $START ]
@@ -46,8 +73,10 @@ do
             export LISTED=`ls "$DESTINATION" | grep $FILE`
             if [ "$LISTED" == "" ]
             then
-              wget $URL -O "$DESTINATION/tmp.jpg" && mv "$DESTINATION/tmp.jpg" "$DESTINATION/$FILE"
+              wget $URL -O "$DESTINATION/$TEMPFILE" && mv "$DESTINATION/$TEMPFILE" "$DESTINATION/$FILE"
               #echo $URL
+              #if a download fails we end up with empty temp files - remove them
+              rm -f "$DESTINATION/$TEMPFILE"
             fi
           fi
         fi
@@ -55,4 +84,4 @@ do
     done
   done
 done
-#echo $PREFIX
+

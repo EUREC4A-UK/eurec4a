b4ee7c2a51f262e8bcbc237d4c6231e406f2c560
Phil Rosenberg <p.d.rosenberg@gmail.com>
2019-11-26 17:32:09 +0000

    Added shell scrit allGoes.sh
    This calls other scripts to download and process GOES images

---
+++ scripts/allGoes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Bash script to download and crop sets of GOES images
+#basically combines all the grabbing of GOES data into
+#one script
+
+if [ $# -eq 1 ]
+then
+  if [ "$1" == "--help" ] || [ "$1" == "-h" ]
+  then
+    echo Usage: allGoes.sh destination_directory
+    exit 0
+  fi
+fi
+
+if [ $# -ne 1 ]
+then
+  echo Incorrect number of parameters. Run getGoes.sh --help to see usage information
+  exit 1
+fi
+
+DESTINATION=$1
+SCRIPTDIRECTORY=`dirname $0`
+
+$SCRIPTDIRECTORY/getGoes.sh GEOCOLOR $DESTINATION/GEOCOLOR
+$SCRIPTDIRECTORY/cropGoes.sh $DESTINATION/GEOCOLOR $DESTINATION/GEOCOLOR/crop
+
+
